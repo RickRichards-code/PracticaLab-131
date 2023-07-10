37934a836b776f7b750b024f09a01eb9f6ada661
Enrique julio Alvarado <rick.2099.xd@gmail.com>
2023-07-10 09:07:00 -0400

    Cola Circular Objetosss\

---
+++ src/Colas/Libro.java
@@ -0,0 +1,15 @@
+package Colas;public class Libro {
+    private String nombre;
+    public Libro(){
+    }
+    public void leer(){
+        System.out.println("\n---------- ingrese datos ----------");
+        System.out.print("ingrese nombre:");
+        System.out.print("ingrese :");
+    }
+    public void mostrar(){
+        System.out.println("\n---------- datos ----------");
+        System.out.println("nombre: ");
+        System.out.println(": ");
+    }
+}
