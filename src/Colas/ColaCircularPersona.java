4c6d8cfa9e934debf7d130b6b72e079375514401
Enrique julio Alvarado <rick.2099.xd@gmail.com>
2023-07-10 08:57:43 -0400

    Cola Circular Objetos

---
+++ src/Colas/ColaCircularPersona.java
@@ -0,0 +1,15 @@
+package Colas;public class ColaCircularPersona {
+    private String nombre;
+    public ColaCircularPersona(){
+    }
+    public void leer(){
+        System.out.println("\n---------- ingrese datos ----------");
+        System.out.print("ingrese nombre:");
+        System.out.print("ingrese :");
+    }
+    public void mostrar(){
+        System.out.println("\n---------- datos ----------");
+        System.out.println("nombre: ");
+        System.out.println(": ");
+    }
+}
---
+++ src/Colas/Persona.java
@@ -0,0 +1,15 @@
+package Colas;public class Persona {
+    private String nombre;
+    public Persona(){
+    }
+    public void leer(){
+        System.out.println("\n---------- ingrese datos ----------");
+        System.out.print("ingrese nombre:");
+        System.out.print("ingrese :");
+    }
+    public void mostrar(){
+        System.out.println("\n---------- datos ----------");
+        System.out.println("nombre: ");
+        System.out.println(": ");
+    }
+}
